70bbdf11d452a66cdac8f25ff338d3ca410ef652
literat <tomas@litera.me>
2019-11-06 21:35:14 +0100

    feat: Add employees to playground app   * @see https://ionicframework.com/blog/announcing-ionic-react/

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import {
   IonTabs
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import { apps, flash, send } from 'ionicons/icons';
+import { apps, flash, people } from 'ionicons/icons';
 import Tab1 from './pages/Tab1';
 import Tab2 from './pages/Tab2';
 import Tab3 from './pages/Tab3';
@@ -56,8 +56,8 @@ const App: React.FC = () => (
             <IonLabel>Tab Two</IonLabel>
           </IonTabButton>
           <IonTabButton tab="tab3" href="/tab3">
-            <IonIcon icon={send} />
-            <IonLabel>Tab Three</IonLabel>
+            <IonIcon icon={people} />
+            <IonLabel>Employees</IonLabel>
           </IonTabButton>
         </IonTabBar>
       </IonTabs>
--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,17 +1,54 @@
-import React from 'react';
-import { IonHeader, IonToolbar, IonPage, IonTitle, IonContent } from '@ionic/react';
+import { IonAvatar, IonContent, IonHeader, IonItem, IonLabel, IonList, IonPage, IonTitle, IonToolbar, useIonViewWillEnter } from '@ionic/react';
+import React, { useState } from 'react';
+
+interface Person {
+  name: string;
+  email: string;
+  position: string;
+  photo: string;
+}
 
 const Tab3Page: React.FC = () => {
+
+  const [people, setPeople] = useState<Person[]>([]);
+
+  useIonViewWillEnter(async () => {
+    const result = await fetch('https://uifaces.co/api?limit=25', {
+      headers: { 'x-API-KEY': '873771d7760b846d51d025ac5804ab' }
+    });
+    const data = await result.json();
+    setPeople(data);
+  });
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab Three</IonTitle>
+          <IonTitle>Employees</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent />
+      <IonContent>
+        <IonList>
+          {people.map((person, idx) => <EmployeeItem key={idx} person={person} />)}
+        </IonList>
+      </IonContent>
     </IonPage>
   );
 };
+
+const EmployeeItem: React.FC<{ person: Person }> = ({ person }) => {
+  return (
+    <IonItem >
+      <IonAvatar slot="start">
+        {/* eslint-disable-next-line */}
+        <img src={person.photo} alt='' />
+      </IonAvatar>
+      <IonLabel>
+        <h2>{person.name}</h2>
+        <p>{person.position}</p>
+      </IonLabel>
+    </IonItem>
+  );
+}
 
 export default Tab3Page;
